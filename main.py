fc93ffc2d1f295e4cf90635e2308ad5f69a56156
Dhanvant <dhanvantg@gmail.com>
2023-01-06 18:39:19 +0530

    Update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import openai
-from PIL import Image, ImageFont, ImageDraw, ImageTk
+from PIL import Image, ImageFont, ImageDraw
 from fpdf import FPDF
 import random
-import json
-openai.api_key = "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"
 
+openai.api_key = "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"
+pdl = []
 
 def chatbot(prompt):
     response = openai.Completion.create(
@@ -20,98 +20,119 @@ def punc(ques):
     if ques.endswith('?'):
         return ques
     else:
-        return ques+'?'
+        return ques + '?'
 
 
-def run(ques, cl):
-    ques = ques.split('\n')
-    lf = []
-    for i in ques:
+def run(ques, cl, uname, hwrite, app):
 
-        lf.append(punc(i.replace('\r', '')))
-    print(lf)
-    print("Hi! I am Saul The Solver and I SAUL assignments!")
-    idt = {"Name": "",
-           "Suffix": "",
-           "Type": "",
-           "Page": ""}
-    with open("Data/files/user.json", "w") as write_file:
-        json.dump(idt, write_file)
-    with open("Data/files/user.json") as rd:
-        dat = json.load(rd)
-    ast = "Data/assets/img"
-    file1 = open("Data/files/answer.txt", "w")
-    print("SAULING the assignment...")
-    x = 1
-    for q in lf:
-        scl = chatbot(q)
-        while scl.startswith('\n'):
-            scl = scl[1:]
-        file1.write(str(x) + "." + " " + q.capitalize() + "\n" + " " + scl + "\n\n")
-        x += 1
+    def draw(txt, clr, hwrite, line, page, uname, img):
+        if hwrite == '1':
+            ys = 207
+            font = ImageFont.truetype("static/fonts/Danwriting.otf", 65)
+        else:
+            ys = 236
+            font = ImageFont.truetype("static/fonts/JustMeAgainDownHere-Regular.ttf", 60)
+        draw = ImageDraw.Draw(im=img)
+        txt = txt.split("\n")
+        wd = 43
+        ft = ""
+        nl = 0
+        for ln in txt:
+            if len(ln) < wd:
 
-    file1.close()
+                nl += 1
+            elif len(ln) >= wd:
+                wid = len(ln)
+                nt = wid // wd
+                it = 1
+                while nt != 0:
+                    cp = wd * it
+                    rn = cp
+                    while ln[rn] != " ":
+                        rn -= 1
+                    ln = ln[:rn] + "\n" + ln[rn:]
+                    nt -= 1
+                    it += 1
+                nl += 1
+            ft = ft + ln + "\n"
+        ft = ft.split("\n")
+        xs = 214
+        xq = 150
+        yc = 58
+        nl = line
+        np = page
+        cnt = 0
+        nos = 0
+        for fn in ft:
+            if clr:
+                colr = '#1961A3'
+                fn = '   ' + fn
+            else:
+                colr = '#000000'
+                fn = fn.replace(".", ".  ")
+                if nos > 0:
+                    fn = '   ' + fn
+                nos += 1
+            draw.text(xy=(xq, ys + yc * nl), text=fn, font=font, fill=colr)
+            cnt += 1
+            nl += 1
+            if nl >= 25:
+                imp = "Output/" + uname + str(np)
+                img.save(imp + ".png")
+                np += 1
+                print(uname, page)
+                nl = 0
+                if imp + ".png" not in pdl:
+                    pdl.append(imp + ".png")
+                return nl, pdl, np, Image.open("Data/assets/img/ruled1.png")
+            else:
+                imp = "Output/" + uname + str(np)
+                img.save(imp + ".png")
+                if imp + ".png" not in pdl:
+                    pdl.append(imp + ".png")
+        return nl, pdl, np, Image.open("Output/"+uname+str(page)+".png")
+
+    if app == 1:
+        ques = ques.split('\n')
+        lf = []
+        for i in ques:
+            lf.append(punc(i.replace('\r', '')))
+        print(lf)
+        print("Hi! I am Saul The Solver and I SAUL assignments!")
+        ast = "Data/assets/img"
+        file1 = open("Data/files/answer.txt", "w")
+        print("SAULING the assignment...")
+        x = 1
+        for q in lf:
+            scl = chatbot(q)
+            while scl.startswith('\n'):
+                scl = scl[1:]
+            file1.write(str(x) + "." + " " + q.capitalize() + "\n" + " " + scl + "\n\n")
+            x += 1
+        file1.close()
+
+    else:
+        file1 = open("Data/files/answer.txt", "w")
+        file1.write(ques)
+        file1.close()
+    ar = open("Data/files/answer.txt", "r")
+    text = ar.read()
+    text = text.replace('\n\n', '\n')
+    ar.close()
     print("Writing the assignment...")
     # Imposing on Image
-
-    ar = file1 = open("Data/files/answer.txt", "r")
-    text = ar.read()
-    ar.close()
-    img = Image.open("Data/assets/img/ruled1.png")
-    base_width, base_height = img.size[0]-400, img.size[1]
-    font = ImageFont.truetype("Danwriting.otf", 65)
-    draw = ImageDraw.Draw(im=img)
-    txt = text.split("\n")
-    wd = 42
-    ft = ""
-    nl = 0
-    for ln in txt:
-        if len(ln) < wd:
-
-            nl += 1
-        elif len(ln) >= wd:
-            wid = len(ln)
-            nt = wid // wd
-            it = 1
-            while nt != 0:
-                cp = wd*it
-                rn = cp
-                while ln[rn] != " ":
-                    rn -= 1
-                ln = ln[:rn]+"\n"+ln[rn:]
-                nt -= 1
-                it += 1
-            nl += 1
-        ft = ft+ln+"\n"
-    ft = ft.split("\n")
-    xs = 214
-    xq = 140
-    ys = 207
-    yc = 58
+    cnt = 0
     nl = 0
     np = 1
     pdl = []
-    for fn in ft:
-        try:
-            if int(fn[:1]) > 0:
-                ln = fn.replace(".", ".  ")
-                draw.text(xy=(xq, ys + yc * nl), text=ln, font=font, fill='#000000')
-        except:
-            draw.text(xy=(xs, ys + yc * nl), text=fn, font=font, fill='#1961A3')
-        nl+=1
-        if nl >= 25:
-            imp = "Output/pg" + str(np)
-            img.save(imp + ".png")
-            np += 1
-            img = Image.open("Data/assets/img/ruled1.png")
-            draw = ImageDraw.Draw(im=img)
-            nl = 0
-            pdl.append(imp+".png")
+    img = Image.open("Data/assets/img/ruled1.png")
+    for i in text.split('\n'):
+        if cnt % 2:
+            nl, pdl, np, img = draw(i, True, hwrite, nl, np, uname, img)
         else:
-            imp = "Output/pg" + str(np)
-            img.save(imp + ".png")
-            if imp + ".png" not in pdl:
-                pdl.append(imp + ".png")
+            nl, pdl, np, img = draw(i, False, hwrite, nl, np, uname, img)
+        cnt += 1
+
     try:
         if pdl[0] == pdl[1]:
             pdl.pop(1)
@@ -121,7 +142,7 @@ def run(ques, cl):
     # imagelist is the list with all image filenames
     shl = ["l1", "r1"]
     for image in pdl:
-        shd = Image.open("Data/assets/img/"+random.choice(shl)+".png")
+        shd = Image.open("Data/assets/img/" + random.choice(shl) + ".png")
         img = Image.open(image).convert("RGBA")
         img.alpha_composite(shd)
         img.save(image)
@@ -130,9 +151,7 @@ def run(ques, cl):
             bw.save(image)
         pdf.add_page()
         pdf.set_auto_page_break(0)
-        pdf.image(image, w = 200)
-    pdf.output('Output/solved.pdf', "F")
+        pdf.image(image, w=200)
+    pdf.output('Output/' + uname + '.pdf', "F")
     print(pdl)
     print("Successfully Created PDF")
-    n = 0
-    no = 1
